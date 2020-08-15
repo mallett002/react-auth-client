439edd24d5d2720d4892d1a82068a51c7258b3b9
Will Mallett <mallett002@gmail.com>
2020-08-15 11:10:52 -0500

    remove console log

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,6 @@ import {combineReducers} from 'redux';
 import {reducer as formReducer} from 'redux-form';
 import auth from './auth';
 
-console.log(auth);
-
 export default combineReducers({
     auth,
     form: formReducer
